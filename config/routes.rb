22e55b5d4457951f9d5e72f6ddb931cc16ad296b
Nino Raymond <nino.raymond@hec.edu>
2018-12-03 17:51:24 +0100

    finally correct

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   resources :city do
     resources :shops, only: [:index, :show] do
-      resources :events, only: [:new, :create,]
+      resources :events, only: [:index, :new, :create, :edit, :delete]
     end
   end
 
