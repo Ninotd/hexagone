689fb120f7a39eea337959c52ddf8db89cb99a08
Nino Raymond <nino.raymond@hec.edu>
2018-12-03 17:35:48 +0100

    done routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+
+  resources :city do
+    resources :shops, only: [:index, :show] do
+      resources :events, only: [:new, :create, :delete, :]
+    end
+  end
+
+  resources :shops, only: [:new, :create, :edit, :delete]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
