5ae850c926fb460744720fd34367a43359f90739
PaulAntoine Huilizen <paul.antoine.huilizen@gmail.com>
2018-12-04 14:11:08 +0100

    check work with victor on mergers

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,15 +1,10 @@
 class CitiesController < ApplicationController
-  def index
-    @best_events = Event.all.first(3)
-    @best_shops = Shop.all.first(3)
-    @last_events = Event.all.first(3)
-  end
-
   def search_city
     @city = City.find_by(name: params[:query])
     if @city.nil?
       flash[:alert] = "Nous n'avons pas encore référencé cette ville."
       redirect_to root_path
     end
+    @last_events = Event.all.first(3)
   end
 end
