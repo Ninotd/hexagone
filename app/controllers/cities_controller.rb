58c6ae3bf017b67ddcb75300b0c33a2152540167
PaulAntoine Huilizen <paul.antoine.huilizen@gmail.com>
2018-12-04 13:53:59 +0100

    first step work on landing and impacts on city show

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -3,4 +3,12 @@ class CitiesController < ApplicationController
     @best_events = Event.all.first(3)
     @best_shops = Shop.all.first(3)
   end
+
+  def search_city
+    @city = City.find_by(name: params[:query])
+    if @city.nil?
+      flash[:alert] = "Nous n'avons pas encore référencé cette ville."
+      redirect_to root_path
+    end
+  end
 end
--- app/models/city.rb
+++ app/models/city.rb
@@ -1,4 +1,10 @@
 class City < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   has_many :shops, dependent: :destroy
+  has_many :events, through: :shops
+
+  include PgSearch
+  pg_search_scope :search_by_name, against: [:name], using: {
+    tsearch: { prefix: true }
+  }
 end
