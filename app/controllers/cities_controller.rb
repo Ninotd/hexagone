41a40f51b4cab08162cf45ad7773a7f1d8ecf8a8
victor desjardins <desjardins.victor@gmail.com>
2018-12-04 12:21:23 +0100

    event card with hover style

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -2,5 +2,6 @@ class CitiesController < ApplicationController
   def index
     @best_events = Event.all.first(3)
     @best_shops = Shop.all.first(3)
+    @last_events = Event.all.first(3)
   end
 end
