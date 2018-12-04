f02ae48fec78058a404799dea82e7811d92e0afe
PaulAntoine Huilizen <paul.antoine.huilizen@gmail.com>
2018-12-04 17:19:00 +0100

    travaux sur les cards de la show de la ville

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,12 +1,15 @@
 class CitiesController < ApplicationController
+
   def search_city
     @city = City.find_by(name: params[:query])
     if @city.nil?
-      flash[:alert] = "Nous n'avons pas encore référencé cette ville."
+      flash[:alert] = "Nous ne sommes pas encore présents dans cette ville. Revenez plus tard !"
       redirect_to root_path
+      return
     end
     @best_events = @city.events.first(3)
     @incoming_events = incoming_events
+    @recent_events = recent_events
     @months = {
       "1" => 'janvier',
       "2" => 'février',
@@ -23,13 +26,20 @@ class CitiesController < ApplicationController
     }
   end
 
-
-
   private
 
   def incoming_events
-    last_events = Event.where("start_date >= ?", Date.today)
-    last_events_ordered = last_events.order("start_date")
-    return last_events_ordered.first(3)
+    incoming_events = @city.events.where("start_date >= ?", Date.today)
+    incoming_events_ordered = incoming_events.order("start_date")
+    return incoming_events_ordered.first(3)
+  end
+
+  def recent_events
+    # selectionner les events dont la startdate est inferieure a la date du jour
+    recent_events = @city.events.where("start_date <= ?", Date.today)
+    # ordonner ces resultats en descending pour etre au plus proche de la date du jour
+    recent_events_ordered = recent_events.order("start_date DESC")
+    # selectionner les 3 premiers
+    return recent_events_ordered.first(3).reverse
   end
 end
